e6a6d7f866228a1909e4bc0b6cb3ee8c6bd3928e
Pedro Ícaro <160777218+Icrxmee@users.noreply.github.com>
2025-10-11 15:25:35 -0300

    Update PAR ou ÍMPAR.py

--- PAR ou ÍMPAR.py
+++ PAR ou ÍMPAR.py
@@ -1,23 +1,23 @@
 from random import randint
 
-v = 0
+v = 0 #Variável de controle
 
-while True:
+while True: #Começa o laço infinito 
 
-    p = int ( input("Digite um número:"))
-    c = randint(0, 10)
-    t = p + c
+    p = int ( input("Digite um número:")) #Número pelo user
+    c = randint(0, 10) #Sistema randomiza um número de até 10
+    t = p + c 
    
 
-    e = ' '
+    e = ' ' #Faz o While interno rodar.
 
-    while e not in 'PpIi':
+    while e not in 'PpIi': #Uma espécime de tratamento de erro. Enquanto não for inputado o valor correto, ele não finaliza ou continua.
 
-        e = str( input ("ímpar ou Par: ")).strip().upper()[0]
+        e = str( input ("ímpar ou Par: ")).strip().upper()[0] #Formata a escolha do usuário para um padrão de somente a primeira letra e maiúscula.
 
-    print (f"Você jogou {p} e o computador jogou {c}, total de: {t}")
+    print (f"Você jogou {p} e o computador jogou {c}, total de: {t}") #Retorna o resultado das escolha tanto do usuário quanto do sistema.
 
-    if e == 'P':
+    if e == 'P': #Todo o cálculo para retornar quantas vitórias o usuário teve até o sistema parar.
         if t % 2 == 0:
             print ("Você ganhou")
             v += 1
@@ -36,4 +36,5 @@ while True:
     
     print("Vamos jogar novamente...")
 
-print(f"Você ganhou {v}")
+
+print(f"Você ganhou {v}") #Retorna o número de vitórias.
