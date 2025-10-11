d10d99397de21cb9837bfe6004abe8a77e545e1f
Pedro Ícaro <160777218+Icrxmee@users.noreply.github.com>
2025-10-11 15:29:15 -0300

    Update TABUADA COM ENCERRAMENTO.py

--- TABUADA COM ENCERRAMENTO.py
+++ TABUADA COM ENCERRAMENTO.py
@@ -1,10 +1,11 @@
-while True:
-    n = int ( input("Deseja ver a tabuada de qual número:"))
+while True: #Começa o While infinito.
+    n = int ( input("Deseja ver a tabuada de qual número:")) #Usuário escolhe o número que deseja a tabuada.
 
-    if n <= -1:
+    if n <= -1: #Define se por acaso, o número for negativo, o código encerra.
         break
 
-    for i in range(1, 11):
-      print (f"{n} x {i} = {n*i}")
+    for i in range(1, 11): #O For vai de 1 a 10
+      print (f"{n} x {i} = {n*i}") #Múltiplica o número escolhido, pelo index de cada volta do laço.
 
 print("TABUADA ENCERRADA")
+
